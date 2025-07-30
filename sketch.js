88073fb9bae6d621dba7f6e482024f3e5ecedcde
Tjuze <tjuze.biz@gmail.com>
2025-07-30 20:46:52 +0200

    Update sketch.js
    Add Starter

--- sketch.js
+++ sketch.js
@@ -1,1 +1,26 @@
 
+let design = "";
+let mic;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+  // Voice recognition setup
+  mic = new p5.SpeechRec('en-US', gotSpeech);
+  mic.continuous = true;
+  mic.start();
+}
+
+function gotSpeech() {
+  if (mic.resultValue) {
+    design = mic.resultString;
+    console.log(design); // Debug voice input
+  }
+}
+
+function draw() {
+  background(240);
+  textSize(20);
+  text("Say your t-shirt design:", 20, 40);
+  text(design, 20, 80); // Show voice input
+}
